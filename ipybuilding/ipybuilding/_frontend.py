f9854ad4f31b700a75e8b7b2abc03e9992e9ca68
Victor Férat <victor.ferat@live.Fr>
2022-03-02 15:26:24 +0100

    cookiecutter

---
+++ ipybuilding/ipybuilding/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) victor férat, jonathan chambers.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "ipybuilding"
+module_version = "^0.1.0"
